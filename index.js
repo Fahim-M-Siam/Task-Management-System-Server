d5577486be334af41daa836075ad0aaf88174122
Fahime <fahim.mohammad.siam@gmail.com>
2023-12-22 03:48:44 +0600

    Fixed a bug in get method of tasks

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zdityrz.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -29,6 +29,32 @@ async function run() {
     app.post("/tasks", async (req, res) => {
       const taskData = req?.body;
       const result = await taskCollection.insertOne(taskData);
+      res.send(result);
+    });
+    app.get("/allTasks", async (req, res) => {
+      const email = req?.query?.email;
+      const query = { email: email };
+      const result = await taskCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/tasks", async (req, res) => {
+      const email = req?.query?.email;
+      const status = req?.query?.status;
+      const query = { email: email, status: status };
+      const result = await taskCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.put("/tasks/:id", async (req, res) => {
+      const updated = req.body;
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const upsert = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          status: updated.status,
+        },
+      };
+      const result = await taskCollection.updateOne(query, updatedDoc, upsert);
       res.send(result);
     });
 
